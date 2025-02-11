36fc80740f8974bf6724ba6187a0487b654fb7fa
Anton <antonmerkotun@gmail.com>
2025-02-11 12:32:10 +0100

    fix

--- src/components/sections/Ecosystem/Ecosystem.jsx
+++ src/components/sections/Ecosystem/Ecosystem.jsx
@@ -1,7 +1,9 @@
 import { ReactSVG } from 'react-svg';
-import circle from '@/assets/icons/ecosystem-circle.svg';
-import styles from '@/components/sections/Ecosystem/Ecosystem.module.scss';
 import Button from '@/components/ui/Button/Button.jsx';
+import circle from '@/assets/icons/ecosystem-circle.svg';
+import ecosystem from '@/assets/images/ecosystem.png';
+import ecosystemMobile from '@/assets/images/ecosystem-mobile.png';
+import styles from '@/components/sections/Ecosystem/Ecosystem.module.scss';
 
 const Ecosystem = () => {
   return (
@@ -16,14 +18,10 @@ const Ecosystem = () => {
       </div>
 
       <div className={styles.image}>
-        <img src="src/assets/images/ecosystem.png" alt="image" width="979" />
+        <img src={ecosystem} alt="image" width="979" />
       </div>
       <div className={styles.imageMobile}>
-        <img
-          src="src/assets/images/ecosystem-mobile.png"
-          alt="image"
-          width="358"
-        />
+        <img src={ecosystemMobile} alt="image" width="358" />
       </div>
       <div className={styles.circle}>
         <ReactSVG src={circle} />
